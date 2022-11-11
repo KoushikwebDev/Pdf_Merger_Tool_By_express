c9f9a711e3278e3152272fc0427c9448b2fa4609
KoushikwebDev <kou.saha.ami@gmail.com>
2022-11-11 13:01:08 +0530

    ready to deploy

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+const app = express();
+const port = process.env.PORT || 3000;
+app.use("/static", express.static("public"));
+
+const mergePdfs = require("./pdfmerge");
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "templates/index.html"));
+});
+
+app.post("/merge", upload.array("pdfs", 2), async function (req, res, next) {
+  // console.log(req.files);
+  let d = await mergePdfs(
+    path.join(__dirname, req.files[0].path),
+    path.join(__dirname, req.files[1].path)
+  );
+  //   res.send({ data: req.files });
+  res.redirect(`http://localhost:3000/static/${d}.pdf`);
+});
+app.listen(port, () => {
+  console.log(`Server app listening on port ${port}`);
+});
